8af185ff561479f9f19c90f2377e5d203fd70a34
Ryan Will <ryanwilldev@gmail.com>
2018-12-01 12:25:47 -0600

    added time calculations, and JS solutions

--- 2018/day_1/day_1.exs
+++ 2018/day_1/day_1.exs
@@ -1066,3 +1066,9 @@ defmodule Day1 do
     +141482"
   end
 end
+
+IO.puts("The answer to Part 1: #{Day1.part_1()} \n")
+{time, value} = :timer.tc(&Day1.part_2/0)
+time = time / 1000
+IO.puts("The answer to Part 2: #{value}")
+IO.puts("Time to solve Part 2: #{time}ms")
