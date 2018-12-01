e285ce7532cd29b40f6a2f363da12c0c80229538
Ryan Will <ryanwilldev@gmail.com>
2018-12-01 09:44:25 -0600

    solves part two adds readme

--- 2018/day_1/day_1.ex
+++ 2018/day_1/day_1.ex
@@ -3,10 +3,30 @@ defmodule Day1 do
   https://adventofcode.com/2018/day/1
   """
   def part_1 do
-    input |> parse |> Enum.sum()
+    input() |> parse() |> Enum.sum()
   end
 
   def part_2 do
+    input() |> parse() |> part_2(0, %{})
+  end
+
+  def part_2([current | rest] = _frequencies, total, record) do
+    total = current + total
+    record = Map.update(record, total, 1, &(&1 + 1))
+
+    case record[total] do
+      n when n > 1 ->
+        total
+
+      _ ->
+        part_2(rest, total, record)
+    end
+  end
+
+  def part_2([], total, record) do
+    input()
+    |> parse()
+    |> part_2(total, record)
   end
 
   def parse(input) do
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent Of Code
+
+My solutions for the Advent of code challenges in various languages
