fc1f1f45c4d67b6edde77bd8fe169a412b7c7ac2
Ryan Will <ryanwilldev@gmail.com>
2018-12-05 07:02:57 -0600

    solves part one

--- 2018/day_3/day_3.ex
+++ 2018/day_3/day_3.ex
@@ -3,37 +3,33 @@ defmodule AOC.TwentyEighteen.Day3 do
   end
 
   def read_and_parse_input() do
-    test_input()
-
     AOC.read_input("2018/day_3")
-    # |> Enum.take(2)
-    |> Enum.reduce({0, MapSet.new(), MapSet.new()}, fn claim, {overlap, seen, claimed} ->
-      [left_offset, top_offset, width, height] = parse_claim_parts(claim)
+    |> Enum.reduce({MapSet.new(), MapSet.new()}, fn claim, {seen, duplicated_claims} ->
+      current_claim_area = parse_claim_parts(claim) |> get_claimed_area()
 
-      with current_claimed_area <- get_claimed_area(left_offset, top_offset, width, height),
-           #  |> IO.inspect(label: "intersection")
-           overlapping <-
-             MapSet.intersection(current_claimed_area, seen)
-             |> (fn i ->
-                   if MapSet.size(claimed) > 0, do: MapSet.intersection(i, claimed), else: i
-                 end).() do
-        claimed = MapSet.union(claimed, overlapping)
-        seen = MapSet.union(current_claimed_area, seen)
-        {overlap + MapSet.size(overlapping), seen, claimed}
+      cond do
+        MapSet.size(seen) == 0 ->
+          {current_claim_area, duplicated_claims}
+
+        true ->
+          overlap = MapSet.intersection(current_claim_area, seen)
+
+          {MapSet.union(current_claim_area, seen), MapSet.union(duplicated_claims, overlap)}
       end
     end)
-    |> elem(0)
+    |> elem(1)
+    |> MapSet.size()
   end
 
   def parse_claim_parts(claim) do
-    [_, _, offsets, dimensions] = String.split(claim, " ", trim: true)
+    ["#" <> id, _, offsets, dimensions] = String.split(claim, " ", trim: true)
     [left_offset, top_offset] = String.split(offsets, [",", ":"], trim: true)
     [width, height] = String.split(dimensions, "x", trim: true)
 
-    [left_offset, top_offset, width, height] |> Enum.map(&String.to_integer/1)
+    [id, left_offset, top_offset, width, height] |> Enum.map(&String.to_integer/1)
   end
 
-  def get_claimed_area(top_offset, left_offset, width, height) do
+  def get_claimed_area([_id, left_offset, top_offset, width, height]) do
     for x <- (top_offset + 1)..(top_offset + height) do
       for y <- (left_offset + 1)..(left_offset + width) do
         {x, y}
