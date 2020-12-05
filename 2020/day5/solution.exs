29e51eeeca1e49b4a762d1481ef3a881e38ffcb4
Ryan Will <ryanwilldev@gmail.com>
2020-12-05 16:47:15 -0600

    solves part 2

--- 2020/day5/solution.exs
+++ 2020/day5/solution.exs
@@ -1,27 +1,8 @@
 defmodule Day5 do
   @column_space Enum.to_list(0..7)
   @row_space Enum.to_list(0..127)
+  @possible_ids MapSet.new(0..1_023)
 
-  # For example, consider just the first seven characters of FBFBBFFRLR:
-  #
-  #    Start by considering the whole range, rows 0 through 127.
-  #    F means to take the lower half, keeping rows 0 through 63.
-  #    B means to take the upper half, keeping rows 32 through 63.
-  #    F means to take the lower half, keeping rows 32 through 47.
-  #    B means to take the upper half, keeping rows 40 through 47.
-  #    B keeps rows 44 through 47.
-  #    F keeps rows 44 through 45.
-  #    The final F keeps the lower of the two, row 44.
-  #
-  # For example, consider just the last 3 characters of FBFBBFFRLR:
-  #
-  #    Start by considering the whole range, columns 0 through 7.
-  #    R means to take the upper half, keeping columns 4 through 7.
-  #    L means to take the lower half, keeping columns 4 through 5.
-  #    The final R keeps the upper of the two, column 5.
-  #
-  # Every seat also has a unique seat ID: multiply the row by 8, then add the column.
-  #
   def get_input(test?) do
     path = if test?, do: "./test.txt", else: "./day5.txt"
 
@@ -77,9 +58,32 @@ defmodule Day5 do
     end)
   end
 
-  def part2 do
+  def part2(test? \\ false) do
+    scanned_id_set =
+      get_input(test?)
+      |> Enum.map(fn pass ->
+        {row, column} =
+          pass
+          |> split_bits()
+          |> row
+          |> column()
+
+        seat_id(row, column)
+      end)
+      |> MapSet.new()
+
+    missing_ids = MapSet.difference(@possible_ids, scanned_id_set)
+
+    Enum.reduce(missing_ids, nil, fn curr_id, seat_id ->
+      found? = (curr_id + 1) in scanned_id_set and (curr_id - 1) in scanned_id_set
+
+      if found?, do: curr_id, else: seat_id
+    end)
   end
 end
 
 Day5.part1()
 |> IO.inspect(label: "The answer to part 1 is")
+
+Day5.part2()
+|> IO.inspect(label: "The answer to part 2 is")
