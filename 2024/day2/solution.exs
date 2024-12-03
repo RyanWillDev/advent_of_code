ac278aec5141db22c52be6500741b5e0d57425f2
Ryan Will <ryanwilldev@gmail.com>
2024-12-02 18:25:55 -0600

    Solves part 2
    I really wanted a recursive solution

--- 2024/day2/solution.exs
+++ 2024/day2/solution.exs
@@ -1,24 +1,61 @@
 defmodule Solution do
   def part_1 do
-    Enum.reduce(read_input(), 0, fn levels, acc ->
-      if report_safe?(levels), do: acc + 1, else: acc
+    Enum.reduce(read_input(), 0, fn report, acc ->
+      if report_safe?(report), do: acc + 1, else: acc
     end)
   end
 
-  defp report_safe?([a, b | _rest] = levels) do
-    comparator = get_comparator(a, b)
-    report_safe?(levels, comparator)
+  def part_2 do
+    read_input()
+    |> Enum.reduce(0, fn report, acc ->
+      if report_safe?(report, true), do: acc + 1, else: acc
+    end)
   end
 
-  defp report_safe?([a, b], comparator) do
-    safe?(a, b, comparator)
+  defp report_safe?([a, b | _rest] = levels, dampen? \\ false) do
+    compare_fn = get_comparator(a, b)
+    levels_safe?(levels, %{compare_fn: compare_fn, prev: [], dampen?: dampen?})
   end
 
-  defp report_safe?([a, b | rest], comparator) do
-    if safe?(a, b, comparator) do
-      report_safe?([b | rest], comparator)
-    else
-      false
+  defp levels_safe?([a, b], %{compare_fn: compare, dampen?: dampen?, prev: prev}) do
+    cond do
+      safe?(a, b, compare) ->
+        true
+
+      dampen? ->
+        report_safe?(prev ++ [a], false) or
+          report_safe?(prev ++ [b], false)
+
+      true ->
+        false
+    end
+  end
+
+  defp levels_safe?(
+         [a, b | rest] = levels,
+         %{compare_fn: compare, dampen?: dampen?, prev: prev} = ctx
+       ) do
+    cond do
+      safe?(a, b, compare) ->
+        levels_safe?([b | rest], %{ctx | prev: prev ++ [a]})
+
+      dampen? ->
+        without_b = prev ++ [a] ++ rest
+        without_a = prev ++ [b] ++ rest
+
+        case prev do
+          # If prev only contains the first element we can attempt a report without it
+          [_p] ->
+            report_safe?(without_a, false) or
+              report_safe?(without_b, false) or
+              report_safe?(levels, false)
+
+          _ ->
+            report_safe?(without_a, false) or report_safe?(without_b, false)
+        end
+
+      true ->
+        false
     end
   end
 
@@ -39,19 +76,23 @@ defmodule Solution do
     end
   end
 
-  def part_2 do
-  end
-
   defp read_input(test? \\ false) do
     if test? do
+      # Edge cases
       """
-      7 6 4 2 1
-      1 2 7 8 9
-      9 7 6 2 1
-      1 3 2 4 5
-      8 6 4 4 1
-      1 3 6 7 9
+      2 6 9 10 11 12
+      1 2 3 4 5 4
+      1 1 2 3 4 5
       """
+
+      # """
+      # 7 6 4 2 1
+      # 1 2 7 8 9
+      # 9 7 6 2 1
+      # 1 3 2 4 5
+      # 8 6 4 4 1
+      # 1 3 6 7 9
+      # """
     else
       File.read!("input.txt")
     end
@@ -63,4 +104,4 @@ defmodule Solution do
 end
 
 Solution.part_1() |> dbg
-
+Solution.part_2() |> dbg
