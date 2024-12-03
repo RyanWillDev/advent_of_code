0e36f846bab08da726ffc8b330543421ddee5a28
Ryan Will <ryanwilldev@gmail.com>
2024-12-03 17:55:21 -0600

    Add advance_cursor & cleanup

--- 2024/day3/solution.exs
+++ 2024/day3/solution.exs
@@ -15,69 +15,69 @@ defmodule Parser do
 
   defp read_char(%{cursor: ?d, conditionals?: true} = parser), do: read_conditional(parser, [])
 
-  defp read_char(%{input: [h | t]} = parser), do: read_char(%{parser | cursor: h, input: t})
+  defp read_char(parser) do
+    parser
+    |> advance_cursor()
+    |> read_char()
+  end
 
-  defp read_keyword(%{disabled?: true, input: [h | t]} = parser, _buffer) do
-    read_char(%{parser | cursor: h, input: t})
+  defp read_keyword(%{disabled?: true} = parser, _buffer) do
+    parser
+    |> advance_cursor()
+    |> read_char()
   end
 
   @keyword ~c"mul"
-  defp read_keyword(%{cursor: c, input: [h | t]} = parser, buffer) when c in @keyword do
-    parser = %{parser | cursor: h, input: t}
-    read_keyword(parser, buffer ++ [c])
+  defp read_keyword(%{cursor: c} = parser, buffer) when c in @keyword do
+    parser
+    |> advance_cursor()
+    |> read_keyword(buffer ++ [c])
   end
 
-  defp read_keyword(%{cursor: ?(, input: [h | t]} = parser, @keyword) do
-    parser = %{parser | cursor: h, input: t}
-    read_integer(parser, [], [])
+  defp read_keyword(%{cursor: ?(} = parser, @keyword) do
+    parser
+    |> advance_cursor()
+    |> read_integer([], [])
   end
 
   defp read_keyword(parser, _buffer), do: read_char(parser)
 
   @int ?0..?9
-  defp read_integer(%{cursor: c, input: [?, | t]} = parser, buffer, integers)
+  defp read_integer(%{cursor: c, input: [?, | _t]} = parser, buffer, integers)
        when c in @int do
     buffer = buffer ++ [c]
     integer = buffer |> to_string() |> String.to_integer()
 
-    [h | t] = t
-    parser = %{parser | cursor: h, input: t}
-
-    read_integer(parser, [], [integer | integers])
+    parser
+    |> advance_cursor(2)
+    |> read_integer([], [integer | integers])
   end
 
-  @int ?0..?9
-  defp read_integer(%{cursor: c, input: [?) | t], operations: ops} = parser, buffer, integers)
+  defp read_integer(%{cursor: c, input: [?) | _t], operations: ops} = parser, buffer, integers)
        when c in @int do
     buffer = buffer ++ [c]
 
     integers =
       buffer |> to_string() |> String.to_integer() |> List.wrap() |> Enum.concat(integers)
 
-    [h | t] = t
-
-    parser = %{
-      parser
-      | cursor: h,
-        input: t,
-        operations: [fn -> apply(Kernel, :*, integers) end | ops]
-    }
-
-    read_char(parser)
+    %{parser | operations: [fn -> apply(Kernel, :*, integers) end | ops]}
+    |> advance_cursor(2)
+    |> read_char()
   end
 
-  defp read_integer(%{cursor: c, input: [h | t]} = parser, buffer, integers) when c in @int do
-    parser = %{parser | cursor: h, input: t}
-    read_integer(parser, buffer ++ [c], integers)
+  defp read_integer(%{cursor: c} = parser, buffer, integers) when c in @int do
+    parser
+    |> advance_cursor()
+    |> read_integer(buffer ++ [c], integers)
   end
 
   defp read_integer(parser, _buffer, _ints), do: read_char(parser)
 
   @conditional_chars ~c"do'nt"
-  defp read_conditional(%{cursor: c, input: [h | t]} = parser, buffer)
-       when c in @conditional_chars do
-    parser = %{parser | cursor: h, input: t}
-    read_conditional(parser, buffer ++ [c])
+  defp read_conditional(%{cursor: c} = parser, buffer) when c in @conditional_chars do
+    parser
+    |> advance_cursor()
+    |> read_conditional(buffer ++ [c])
   end
 
   defp read_conditional(%{cursor: ?(, input: [h | t]} = parser, buffer) do
@@ -87,11 +87,19 @@ defmodule Parser do
         ~c"don't" -> true
       end
 
-    parser = %{parser | cursor: h, input: t, disabled?: diabled?}
-    read_char(parser)
+    %{parser | cursor: h, input: t, disabled?: diabled?}
+    |> advance_cursor()
+    |> read_char()
   end
 
   defp read_conditional(parser, _buffer), do: read_char(parser)
+
+  defp advance_cursor(parser, steps \\ 1)
+  defp advance_cursor(%{input: [h | t]} = parser, 1), do: %{parser | cursor: h, input: t}
+
+  defp advance_cursor(%{input: [h | t]} = parser, steps) do
+    advance_cursor(%{parser | cursor: h, input: t}, steps - 1)
+  end
 end
 
 defmodule Solution do
