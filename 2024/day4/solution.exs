fac8e9814fb11311fbd227b45c396bc429bbc986
Ryan Will <ryanwilldev@gmail.com>
2024-12-04 23:02:56 -0600

    WIP Part 2
    Works with current x-mas test input
    Needs to match both backward and forward
    Might be easier to start from A

--- 2024/day4/solution.exs
+++ 2024/day4/solution.exs
@@ -1,67 +1,91 @@
 defmodule Solution do
   def part_1 do
     coords = build_coordinates(read_input())
-
     last = coords |> Enum.max() |> elem(0)
+    match_fns = [&vertical_match_count/3, &horizontal_match_count/3, &diagonal_match_count/3]
+    ctx = %{count: 0, match_fns: match_fns, key_letter: "X", string: "XMAS"}
 
-    check_grid(coords, {0, 0}, last, 0)
+    check_grid(coords, {0, 0}, last, ctx)
   end
 
-  defp check_grid(_coords, {current_r, _c}, {max_r, _max_c}, count) when current_r == max_r + 1 do
+  def part_2 do
+    coords = build_coordinates(read_input(true))
+    last = coords |> Enum.max() |> elem(0)
+
+    match_fns = [&x_match_count/3]
+    ctx = %{count: 0, match_fns: match_fns, key_letter: "M", string: "MAS"}
+
+    check_grid(coords, {0, 0}, last, ctx)
+  end
+
+  defp check_grid(_coords, {current_r, _c}, {max_r, _max_c}, %{count: count})
+       when current_r > max_r do
     count
   end
 
-  defp check_grid(coords, {r, c} = location, {_max_r, max_c} = last, count) do
+  defp check_grid(coords, {r, c} = location, {_max_r, max_c} = last, ctx) do
+    %{count: count, match_fns: match_fns, key_letter: key, string: string} = ctx
     letter = Map.get(coords, location, "")
 
     count =
-      if letter == "X" do
-        Enum.reduce(
-          [&vertical_match_count/2, &horizontal_match_count/2, &diagonal_match_count/2],
-          count,
-          fn
-            match_fn, count ->
-              match_fn.(coords, location) + count
-          end
-        )
+      if letter == key do
+        Enum.reduce(match_fns, count, fn
+          match_fn, count ->
+            match_fn.(coords, location, string) + count
+        end)
       else
         count
       end
 
     location = if c == max_c, do: {r + 1, 0}, else: {r, c + 1}
 
-    check_grid(coords, location, last, count)
+    check_grid(coords, location, last, %{ctx | count: count})
   end
 
-  defp vertical_match_count(coords, {c, r}) do
+  defp vertical_match_count(coords, {c, r}, string) do
     coords
-    |> get_letters({&Kernel.+(&1, c), fn _ -> r end})
-    |> Enum.count(&check_match/1)
+    |> get_letters(num_moves(string), {&Kernel.+(&1, c), fn _ -> r end})
+    |> Enum.count(&check_match(&1, string))
   end
 
-  defp horizontal_match_count(coords, {c, r}) do
+  defp horizontal_match_count(coords, {c, r}, string) do
     coords
-    |> get_letters({fn _ -> c end, &Kernel.+(&1, r)})
-    |> Enum.count(&check_match/1)
+    |> get_letters(num_moves(string), {fn _ -> c end, &Kernel.+(&1, r)})
+    |> Enum.count(&check_match(&1, string))
   end
 
-  defp diagonal_match_count(coords, {c, r}) do
+  defp diagonal_match_count(coords, {c, r}, string) do
     right_slant =
       coords
-      |> get_letters({&Kernel.+(&1, c), &Kernel.+(&1, r)})
-      |> Enum.count(&check_match/1)
+      |> get_letters(num_moves(string), {&Kernel.+(&1, c), &Kernel.+(&1, r)})
+      |> Enum.count(&check_match(&1, string))
 
     left_slant =
       coords
-      |> get_letters({&Kernel.+(&1 * -1, c), &Kernel.+(&1, r)})
-      |> Enum.count(&check_match/1)
+      |> get_letters(num_moves(string), {&Kernel.+(&1 * -1, c), &Kernel.+(&1, r)})
+      |> Enum.count(&check_match(&1, string))
 
     left_slant + right_slant
   end
 
-  defp get_letters(coords, {move_c, move_r}) do
+  defp x_match_count(coords, {c, r}, string) do
+    right_slant =
+      coords
+      |> get_letters(num_moves(string), {&Kernel.+(&1, c), &Kernel.+(&1, r)})
+      |> Enum.count(&check_match(&1, string))
+
+    left_slant =
+      coords
+      |> get_letters(num_moves(string), {&Kernel.+(&1, c + 2), &Kernel.+(&1 * -1, r)})
+      |> dbg
+      |> Enum.count(&check_match(&1, string))
+
+    if left_slant + right_slant == 2, do: 1, else: 0
+  end
+
+  defp get_letters(coords, num_moves, {move_c, move_r}) do
     {before_leters, after_letters} =
-      Enum.reduce(0..3, {[], []}, fn n, {before_letters, after_letters} ->
+      Enum.reduce(0..num_moves, {[], []}, fn n, {before_letters, after_letters} ->
         before_coords = {move_c.(n * -1), move_r.(n * -1)}
         after_coords = {move_c.(n), move_r.(n)}
 
@@ -76,8 +100,10 @@ defmodule Solution do
     end)
   end
 
-  defp check_match("XMAS"), do: true
-  defp check_match(_), do: false
+  defp check_match(string, string), do: true
+  defp check_match(_, _), do: false
+
+  defp num_moves(string), do: String.length(string) - 1
 
   defp build_coordinates(rows) do
     rows
@@ -108,6 +134,12 @@ defmodule Solution do
       MAMMMXMMMM
       MXMXAXMASX
       """
+
+      """
+      MBS
+      BAB
+      MBS
+      """
     else
       File.read!("input.txt")
     end
@@ -117,3 +149,4 @@ defmodule Solution do
 end
 
 Solution.part_1() |> dbg
+# Solution.part_2() |> dbg
